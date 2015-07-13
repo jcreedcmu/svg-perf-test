570b16108ac6590b53525968596f87212242eb39
Jason Reed <jcreed@gmail.com>
2015-07-12 20:25:31 -0400

    oh this actually works well

--- a.js
+++ a.js
@@ -1,14 +1,40 @@
 // data in a.json generated through
 // potrace a.pbm -a 0 -b geojson
 var t = Date.now();
+
+function svg(tag) {
+  return $(document.createElementNS('http://www.w3.org/2000/svg', tag));
+}
+
+function CompoundPath() {
+  this.path = "";
+}
+
+CompoundPath.prototype.moveTo = function(x, y) {
+  this.path += "M" + x + " " + y;
+}
+
+CompoundPath.prototype.lineTo = function(x, y) {
+  this.path += "L" + x + " " + y;
+}
+
+CompoundPath.prototype.closePath = function() {
+  this.path += "z";
+}
+
+CompoundPath.prototype.svg = function() {
+  var path = svg('path');
+  path.attr({d: this.path});
+  return path;
+}
+
+
+var scale = 4;
+var yoff = 550;
+
+
 $.ajax('a.json',{success: function(json) {
   var data = JSON.parse(json);
-
-
-
-  data.features.forEach(function(feature) {
-    feature.geometry.coordinates[0];
-  });
 
   paper.setup(document.getElementById("c"));
 
@@ -22,10 +48,6 @@ $.ajax('a.json',{success: function(json) {
 
     feature.geometry.coordinates.forEach(function(pathc) {
 
-      var scale = 2;
-      var yoff = 1150;
-
-
 
       pathc.forEach(function(vert, ix) {
 	if (ix == 0)
@@ -38,6 +60,57 @@ $.ajax('a.json',{success: function(json) {
     });
   });
   paper.view.draw();
+  console.log(Date.now() - t);
+
+  data.features.forEach(function(feature) {
+    var path = new CompoundPath();
+
+//    path.fillColor = 'black';
+
+    feature.geometry.coordinates.forEach(function(pathc) {
+
+
+
+
+      pathc.forEach(function(vert, ix) {
+	if (ix == 0)
+	  path.moveTo(vert[0] / scale, yoff - vert[1] / scale);
+	else
+	  path.lineTo(vert[0] / scale, yoff - vert[1] / scale);
+
+      });
+      path.closePath(false);
+    });
+
+    var psvg = path.svg();
+    psvg.attr({fill: "black"});
+    psvg.appendTo($("#s"));
+
+  });
 
   console.log(Date.now() - t);
 }});
+
+$svg = $("#s");
+
+var basex = 0;
+var basey = 0;
+$svg.on('mousedown', function(e) {
+  var th = $(this);
+  var x = e.pageX;
+  var y = e.pageY;
+  var membasex = basex;
+  var membasey = basey;
+  $(document).on('mousemove.drag', function(e) {
+    var t = Date.now();
+
+    $svg.attr('transform', 'translate(' + (membasex + e.pageX - x) + ', ' +
+     (membasey + e.pageY - y) + ')');
+
+  });
+  $(document).on('mouseup.drag', function(e) {
+    basex += e.pageX - x;
+    basey += e.pageY - y;
+    $(document).off('.drag');
+  });
+});

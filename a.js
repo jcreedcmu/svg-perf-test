629d2c3df4902912397347201a3a9714b538391c
Jason Reed <jcreed@gmail.com>
2015-07-15 21:57:02 -0400

    use rtree to selectively draw parts

--- a.js
+++ a.js
@@ -6,6 +6,8 @@ g_data = null;
 $.ajax('b.json',{success: function(json) {
   count = 0;
   g_data = JSON.parse(json);
+  rt = new RTree(10);
+  rt.geoJSON(JSON.parse(json));
 
   camera = {x: 180, y: 600, scale: 0.2};
 
@@ -17,35 +19,41 @@ $.ajax('b.json',{success: function(json) {
   render();
 }});
 
-$svg = $("#svg");
-$s = $("#s");
-
 function render() {
-
+  var t = Date.now();
   d.clearRect(0,0,w,h);
+  d.strokeStyle = "red";
+  var OFFSET = 200;
+  d.strokeRect(OFFSET + 0.5,OFFSET + 0.5,w-2*OFFSET,h-2*OFFSET);
+  d.strokeStyle = "black";
+  var items = rt.bbox((OFFSET-camera.x) / camera.scale, ((h-OFFSET) - camera.y) / -camera.scale,
+		      ((w-OFFSET) - camera.x) / camera.scale, (OFFSET-camera.y) / -camera.scale  );
+  console.log("items ", items.length);
 
   d.save();
   d.translate(camera.x, camera.y);
   d.scale(camera.scale, camera.scale);
-  g_data.forEach(function(feature) {
+  items.forEach(function(feature) {
     d.beginPath();
-    feature.coords.forEach(function(vert, ix) {
+     feature.geometry.coordinates.forEach(function(vert, ix) {
       if (ix == 0)
-	d.moveTo(vert[0] ,  - vert[1] );
+    	d.moveTo(vert[0] ,  - vert[1] );
       else
-	d.lineTo(vert[0] ,  - vert[1] );
+    	d.lineTo(vert[0] ,  - vert[1] );
     });
 
-    d.fillStyle = "rgba(0,0,0,0.1)";
-    d.fill();
+    // d.fillStyle = "rgba(0,0,0,0.1)";
+    // d.fill();
     d.lineWidth = 0.5 / camera.scale;
     d.stroke();
   });
   d.restore();
+
+
   // $s.attr('transform', 'translate(' + camera.x + ', ' + camera.y +
   // 	  ') scale(' + camera.scale + ')');
 
-
+  console.log(Date.now() - t);
 }
 
 $(c).on('mousewheel', function(e) {
--- break.js
+++ break.js
@@ -4,21 +4,23 @@ var fs = require("fs");
 var g_data = JSON.parse(fs.readFileSync("a.json"));
 CHUNK_SIZE = 100;
 
+
 var features = [];
+var out = {type: "FeatureCollection", features: features};
 g_data.features.forEach(function(feature) {
   feature.geometry.coordinates.forEach(function(pathc) {
     var chunks = Math.ceil(pathc.length / CHUNK_SIZE);
     for (var i = 0; i < chunks; i++) {
-      var feature = {type: "raw_way", coords: []};
+      var feature = {type: "Feature", geometry: {type: "LineString", coordinates: []}};
       features.push(feature);
       for (var j = 0; j < CHUNK_SIZE + 1; j++) {
 	var n = i * CHUNK_SIZE + j;
 	if (n < pathc.length) {
-	  feature.coords.push(pathc[n]);
+	  feature.geometry.coordinates.push(pathc[n]);
 	}
       }
     }
   });
 });
 
-console.log(JSON.stringify(features));
+console.log(JSON.stringify(out, null, 2));

a280de1f144922cdaacf71ba20d26ef16e7ed0e5
Jason Reed <jcreed@gmail.com>
2015-07-19 19:34:33 -0400

    can edit image coordinates

--- a.js
+++ a.js
@@ -3,11 +3,19 @@
 
 
 g_data = null;
-$.ajax('b.json',{success: function(json) {
+var ld = new Loader();
+ld.add(json_file('b'));
+g_curImgName = 1197;
+function image_url() {
+  return 'file:///home/jcreed/art/whatever/' + g_curImgName + '.png';
+}
+ld.add(image(image_url(), 'overlay'));
+ld.done(function(data) {
   count = 0;
-  g_data = JSON.parse(json);
-//  rt = new RTree(10);
-//  rt.geoJSON(JSON.parse(json));
+  assets = this;
+  g_data = assets.src.b;
+  //  rt = new RTree(10);
+  //  rt.geoJSON(JSON.parse(json));
 
   camera = {x: 180, y: 600, scale: 0.2};
 
@@ -17,7 +25,7 @@ $.ajax('b.json',{success: function(json) {
   h = c.height = innerHeight;
 
   render();
-}});
+});
 
 function inv_xform(xpix, ypix) {
   return {x:(xpix-camera.x) / camera.scale, y:(ypix - camera.y) / -camera.scale};
@@ -28,6 +36,49 @@ function xform(xworld, yworld) {
 }
 
 var z = 0;
+g_allStates = (localStorage.allStates != null) ? JSON.parse(localStorage.allStates) :
+  {"1119":{"scale":4096,"x":-0.546875,"y":-3216.171875},
+   "1120":{"scale":8192,"x":-0.546875,"y":-3216.171875},
+   "1121":{"scale":2048,"x":1951.015625,"y":-1262.421875},
+   "1122":{"scale":4096,"x":1986.015625,"y":-1242.421875},
+   "1123":{"scale":1024,"x":2721.953125,"y":-482.109375},
+   "1124":{"scale":64,"x":2829.296875,"y":-372.734375},
+   "1128":{"scale":4096,"x":-0.390625,"y":-3217.734375},
+   "1151":{"scale":1024,"x":2495.625,"y":-855.625},
+   "1154":{"scale":2048,"x":0,"y":-3215},
+   "1155":{"scale":2048,"x":0,"y":-3215},
+   "1156":{"scale":2048,"x":0,"y":-3215},
+   "1160":{"scale":2048,"x":2453.75,"y":-798.75},
+   "1161":{"scale":2048,"x":2453.75,"y":-798.75},
+   "1163":{"scale":1024,"x":2495,"y":-806.25},
+   "1164":{"scale":512,"x":2495,"y":-806.25},
+   "1166":{"scale":1024,"x":2495,"y":-806.25},
+   "1167":{"scale":1024,"x":2495,"y":-806.25},
+   "1170":{"scale":2048,"x":-0.9375,"y":-3216.25},
+   "1171":{"scale":8192,"x":-0.9375,"y":-3216.25},
+   "1173":{"scale":2048,"x":-0.9375,"y":-3216.25},
+   "1174":{"scale":2048,"x":-0.9375,"y":-3216.25},
+   "1176":{"scale":2048,"x":82.8125,"y":-2658.125},
+   "1177":{"scale":2048,"x":-1.25,"y":-3215.9375},
+   "1179":{"scale":1024,"x":756.25,"y":-653.4375},
+   "1180":{"scale":2048,"x":-0.3125,"y":-3216.40625},
+   "1181":{"scale":64,"x":742.03125,"y":-2393.984375},
+   "1182":{"scale":8,"x":827.67578125,"y":-2382.91015625},
+   "1194":{"scale":1024,"x":587.36328125,"y":-3182.91015625},
+   "1195":{"scale":128,"x":1166.66015625,"y":-3071.11328125},
+   "1196":{"scale":512,"x":3301.97265625,"y":-1350.64453125},
+   "1197":{"scale":1024,"x":51.34765625,"y":-1963.76953125},
+   "1198":{"scale":1024,"x":380.17578125,"y":-2540.09765625},
+   "1199":{"scale":1024,"x":2044.55078125,"y":-3215.72265625},
+   "1200":{"scale":1024,"x":0.25390625,"y":-3215.80078125},
+   "1201":{"scale":1024,"x":0.25390625,"y":-3215.80078125},
+   "1202":{"scale":1024,"x":-153.33984375,"y":-3237.98828125},
+   "1203":{"scale":512,"x":3377.28515625,"y":-437.98828125},
+   "1204":{"scale":512,"x":376.03515625,"y":-1594.08203125},
+   "1205":{"scale":128,"x":909.31640625,"y":-1391.26953125},
+   "1207":{"scale":4096,"x":-0.68359375,"y":-3216.26953125}};
+g_imageState = clone(g_allStates[g_curImgName]);
+
 function render() {
   var t = Date.now();
   d.fillStyle = "#def";
@@ -44,13 +95,14 @@ function render() {
   var ip2 = inv_xform(w-OFFSET, h-OFFSET);
   //var items = rt.bbox(ip1.x, ip2.y, ip2.x, ip1.y);
 
+  d.save();
+  d.translate(camera.x, camera.y);
+  d.scale(camera.scale, camera.scale);
   _.each(g_data.objects, function(object, k) {
     var arc_id_lists = object.arcs;
     var arcs = g_data.arcs;
 
-    d.save();
-    d.translate(camera.x, camera.y);
-    d.scale(camera.scale, camera.scale);
+
     z = 0;
     d.beginPath();
     arc_id_lists.forEach(function(arc_id_list) {
@@ -108,45 +160,111 @@ function render() {
     d.stroke();
     d.fillStyle = "white"; // k == "feature0" ? "#fed" : "white";
     d.fill();
-
-
-    d.restore();
-
   });
+
+  d.globalAlpha = 0.5;
+  var ovr = assets.img.overlay;
+  d.drawImage(ovr, 0, 0, ovr.width,
+	      ovr.height, g_imageState.x, g_imageState.y,
+	      ovr.width * g_imageState.scale / 1024,
+	      ovr.height * g_imageState.scale / 1024);
+  d.globalAlpha = 1;
+  d.restore();
+
+
   // $s.attr('transform', 'translate(' + camera.x + ', ' + camera.y +
   // 	  ') scale(' + camera.scale + ')');
 
-//  console.log(Date.now() - t);
+  //  console.log(Date.now() - t);
 }
 
 $(c).on('mousewheel', function(e) {
-  var x = e.pageX;
-  var y = e.pageY;
-  var zoom = 2;
-  if (e.originalEvent.wheelDelta < 0) {
-    zoom = 1/zoom;
+  if (e.ctrlKey) {
+    if (e.originalEvent.wheelDelta < 0) {
+      g_imageState.scale /= 2;
+    }
+    else {
+      g_imageState.scale *= 2;
+    }
+    render();
+    e.preventDefault();
   }
-  camera.x = zoom * (camera.x - x) + x;
-  camera.y = zoom * (camera.y - y) + y;
-  camera.scale *= zoom;
-  render();
+  else {
+    var x = e.pageX;
+    var y = e.pageY;
+    var zoom = 2;
+    if (e.originalEvent.wheelDelta < 0) {
+      zoom = 1/zoom;
+    }
+    camera.x = zoom * (camera.x - x) + x;
+    camera.y = zoom * (camera.y - y) + y;
+    camera.scale *= zoom;
+    render();
+  }
 });
 $(c).on('mousedown', function(e) {
   var th = $(this);
   var x = e.pageX;
   var y = e.pageY;
-  var membasex = camera.x;
-  var membasey = camera.y;
-  $(document).on('mousemove.drag', function(e) {
-    var t = Date.now();
+  if (e.ctrlKey) {
+    var membasex = g_imageState.x;
+    var membasey = g_imageState.y;
+    $(document).on('mousemove.drag', function(e) {
+      g_imageState.x = membasex + (e.pageX - x) / camera.scale;
+      g_imageState.y = membasey + (e.pageY - y) / camera.scale;
+      render();
+    });
+    $(document).on('mouseup.drag', function(e) {
+      $(document).off('.drag');
+    });
 
-    camera.x = membasex + e.pageX - x;
-    camera.y = membasey + e.pageY - y;
-    render();
+  }
+  else {
+    var membasex = camera.x;
+    var membasey = camera.y;
+    $(document).on('mousemove.drag', function(e) {
+      var t = Date.now();
+
+      camera.x = membasex + e.pageX - x;
+      camera.y = membasey + e.pageY - y;
+      render();
 
 
-  });
-  $(document).on('mouseup.drag', function(e) {
-    $(document).off('.drag');
-  });
+    });
+    $(document).on('mouseup.drag', function(e) {
+      $(document).off('.drag');
+    });
+  }
 });
+$(document).on('keypress', function(e) {
+  if (e.charCode == 18 + 96) { // r
+    report();
+  }
+  if (e.charCode == 44) { // <
+    g_curImgName--;
+    reload_img();
+  }
+  if (e.charCode == 46) { // >
+    g_curImgName++;
+    reload_img();
+  }
+  //  console.log(e.charCode);
+});
+
+function report() {
+  g_allStates[g_curImgName] = clone(g_imageState);
+  localStorage.allStates = JSON.stringify(g_allStates);
+  // {pos: [g_imageState.x, g_imageState.y], scale: g_imageState.scale};
+  console.log(JSON.stringify(g_allStates));
+}
+
+function reload_img() {
+  assets.img.overlay.src = image_url();
+  assets.img.overlay.onload = function() {
+    var new_state = g_allStates[g_curImgName];
+    if (new_state != null) {
+      g_imageState = clone(new_state);
+    }
+    render();
+  }
+}

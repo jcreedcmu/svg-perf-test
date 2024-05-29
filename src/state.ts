ecfbe7e4d9e5ff281afe4d08627a16961ddd397f
Jason Reed <jcreed@gmail.com>
2024-05-29 15:51:35 -0400

    Rewrite camera state to be functional

--- src/state.ts
+++ src/state.ts
@@ -1,70 +1,71 @@
 import { Point, Camera } from './types';
 import { clone, scale_of_zoom } from './util';
+import { produce } from 'immer';
 
 export type CameraData = {
   origin: Point,
   camera: Camera,
 };
 
-export class CameraState {
-  data: CameraData;
-
-  constructor() {
-    let camera: Camera = { x: -432.125, y: 3321.875, zoom: 4 };
-    if (localStorage.camera != null) {
-      camera = JSON.parse(localStorage.camera);
-    }
-    this.data = { origin: { x: 0, y: 0 }, camera };
+export function mkCameraData(): CameraData {
+  let camera: Camera = { x: -432.125, y: 3321.875, zoom: 4 };
+  if (localStorage.camera != null) {
+    camera = JSON.parse(localStorage.camera);
   }
+  return { origin: { x: 0, y: 0 }, camera };
 
-  camera() {
-    const c = clone(this.data.camera);
-    c.x -= this.data.origin.x;
-    c.y -= this.data.origin.y;
+}
 
-    return { ...c, scale: () => scale_of_zoom(c.zoom) };
-  }
+export function getCamera(data: CameraData): Camera {
+  return produce(data.camera, d => {
+    d.x -= data.origin.x;
+    d.y -= data.origin.y;
+  });
+}
 
-  zoom(x: number, y: number, zoom: number) {
-    var zoom2 = Math.pow(2, zoom);
-    const s = clone(this.data);
+export function doZoom(data: CameraData, x: number, y: number, zoom: number): CameraData {
+  var zoom2 = Math.pow(2, zoom);
+  return storeCam(produce(data, s => {
     s.camera.x = zoom2 * (s.camera.x - x) + x;
     s.camera.y = zoom2 * (s.camera.y - y) + y;
     s.camera.zoom = s.camera.zoom + zoom;
-    this.data = s;
-    this.store_cam();
-  }
+  }));
+}
 
-  store_cam() {
-    localStorage.camera = JSON.stringify(this.data.camera);
-  }
+export function setCam(data: CameraData, x: number, y: number): CameraData {
+  return storeCam(produce(data, s => {
+    s.camera = { x, y, zoom: data.camera.zoom };
+  }));
 
-  set_cam(x: number, y: number) {
-    this.data = clone(this.data);
-    this.data.camera = { x, y, zoom: this.data.camera.zoom };
-    this.store_cam();
-  }
+}
 
-  inc_cam(dx: number, dy: number) {
-    const s = clone(this.data);
-    s.camera.x = s.camera.x + dx;
-    s.camera.y = s.camera.y + dy;
-    this.data = s;
-    this.store_cam();
-  }
+export function incCam(data: CameraData, dx: number, dy: number): CameraData {
+  return storeCam(produce(data, s => {
+    s.camera.x += dx;
+    s.camera.y += dy;
+  }));
+}
 
-  set_origin(x: number, y: number) {
-    this.data.origin.x = x;
-    this.data.origin.y = y;
-  }
+export function setOrigin(data: CameraData, x: number, y: number): CameraData {
+  return produce(data, s => {
+    s.origin.x = x;
+    s.origin.y = y;
+  });
+}
 
-  get_origin() {
-    return this.data.origin;
-  }
+export function getOrigin(data: CameraData): Point {
+  return data.origin;
+}
 
-  inc_origin(dx: number, dy: number) {
-    this.data.origin.x += dx;
-    this.data.origin.y += dy;
+export function incOrigin(data: CameraData, dx: number, dy: number): CameraData {
+  return storeCam(produce(data, s => {
+    s.origin.x += dx;
+    s.origin.y += dy;
+  }));
+}
 
-  }
+
+function storeCam(data: CameraData): CameraData {
+  localStorage.camera = JSON.stringify(data.camera);
+  return data;
 }

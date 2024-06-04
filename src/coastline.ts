cbbc3ae26240283d7de0319fdce00874aa369d3f
Jason Reed <jcreed@gmail.com>
2024-06-04 09:48:08 -0400

    Factor out coastline render function

--- src/coastline.ts
+++ src/coastline.ts
@@ -174,6 +174,151 @@ function sortBy<T>(items: T[], score: (x: T) => number): T[] {
     .map(x => x.item);
 }
 
+function render(rc: RenderCtx, arcStore: ArcStore, labelStore: LabelStore) {
+  const { d, cameraData, bbox_in_world, us, mode } = rc;
+  function visible(x: Poly): boolean {
+    if (x.properties.t == "road" && !us.layers.road) return false;
+    if (x.properties.t == "boundary" && !us.layers.boundary) return false;
+    return true;
+  }
+
+  const scale = scale_of_camera(cameraData);
+  const arcs_to_draw_vertices_for: Zpoint[][] = [];
+  const salients: { props: RoadProps, pt: Point }[] = [];
+  const rawFeatures = tsearch(arcStore.rt, bbox_in_world).filter(visible);
+  const baseFeatures = sortBy(rawFeatures, x => {
+    let z = 0;
+    const p: PolyProps = x.properties;
+    if (p.t == "natural" && p.natural == "lake") z = 1;
+    if (p.t == "natural" && p.natural == "mountain") z = 1;
+    if (p.t == "city") z = 1;
+    if (p.t == "road" && p.road == "highway") z = 2;
+    if (p.t == "road" && p.road == "street") z = 3;
+    if (p.t == "boundary") z = 4;
+    return z;
+  });
+
+  // Not sure what this is for
+  let extra = baseFeatures.map(x => {
+    if (x.properties.t == "road" && x.properties.road == "street")
+      return { ...x, properties: { ...x.properties, road: "street2" } }
+  }).filter(x => x);
+
+  // Not sure why this cast
+  const features = baseFeatures.concat(extra as any);
+
+  d.save();
+  {
+    canvasIntoWorld(d, cameraData);
+
+    d.strokeStyle = "black";
+    d.lineJoin = "round";
+    features.forEach(ob => {
+      const arc_spec_list = ob.arcs;
+
+      d.lineWidth = 0.9 / scale;
+      d.beginPath();
+
+      const first_point = arcStore.getArc(arc_spec_list[0]).points[0].point;
+      d.moveTo(first_point.x, first_point.y);
+
+      let curpoint = first_point;
+      let n = 0;
+
+      arc_spec_list.forEach(spec => {
+        const arc = arcStore.getArc(spec);
+        let this_arc = arc.points;
+        let arc_bbox = arc.bbox;
+        if (DEBUG_BBOX) {
+          d.lineWidth = 1.5 / scale;
+          d.strokeStyle = colors.debug;
+          d.strokeRect(arc_bbox.minX, arc_bbox.minY,
+            arc_bbox.maxX - arc_bbox.minX,
+            arc_bbox.maxY - arc_bbox.minY);
+        }
+
+        if (this_arc.length < 2) {
+          throw "arc " + spec + " must have at least two points";
+        }
+
+        const rect_intersect = bbox_in_world[0] < arc_bbox.maxX
+          && bbox_in_world[2] > arc_bbox.minX
+          && bbox_in_world[3] > arc_bbox.minY
+          && bbox_in_world[1] < arc_bbox.maxY;
+
+        if (!rect_intersect) {
+          // The bounding box of this arc is entirely off-screen.
+          // Draw as simplified as possible; just one line segment
+          // from beginning to end.
+          this_arc = [this_arc[0], this_arc[this_arc.length - 1]];
+        }
+
+        if (rect_intersect && zoom_of_camera(cameraData) >= 6) {
+          // draw individual vertices if we're at least partially
+          // on-screen, and also quite zoomed in.
+          arcs_to_draw_vertices_for.push(this_arc);
+        }
+
+        this_arc.forEach(({ point: vert, z }, ix) => {
+          if (ix == 0) return;
+
+          let draw = false;
+
+          // draw somewhat simplified
+          if (zoom_of_camera(cameraData) >= 6 || above_simp_thresh(z, scale))
+            draw = true;
+          if (ix == this_arc.length - 1)
+            draw = true;
+          if (draw) {
+            d.lineTo(vert.x, vert.y);
+            if (ob.properties.t == "road" && ob.properties.road == "highway" && n % 10 == 5) {
+              salients.push({
+                props: ob.properties,
+                pt: { x: (vert.x + curpoint.x) / 2, y: (vert.y + curpoint.y) / 2 }
+              });
+            }
+            curpoint = vert;
+            n++;
+          }
+
+        });
+      });
+      realize_path(d, us, ob.properties, cameraData);
+    });
+
+    // draw vertices
+    d.lineWidth = 1.5 / scale;
+    if (mode != "Pan") {
+      d.strokeStyle = "#333";
+      d.fillStyle = "#ffd";
+      let vert_size = 5 / scale;
+      arcs_to_draw_vertices_for.forEach(arc => {
+        arc.forEach(({ point: vert, z }: Zpoint, n: number) => {
+          if (z > 1000000 || zoom_of_camera(cameraData) > 10) {
+            d.fillStyle = z > 1000000 ? "#ffd" : "#f00";
+            d.strokeRect(vert.x - vert_size / 2, vert.y - vert_size / 2, vert_size, vert_size);
+            d.fillRect(vert.x - vert_size / 2, vert.y - vert_size / 2, vert_size, vert_size);
+          }
+        });
+      });
+    }
+  }
+  d.restore();
+
+  // doing this outside the d.save/d.restore because it involves
+  // text, which won't want the negative y-transform
+  salients.forEach(salient => {
+    realize_salient(d, salient.props, cameraData, salient.pt);
+  });
+
+  // render labels
+  if (zoom_of_camera(cameraData) < 1) return;
+  d.lineJoin = "round";
+  tsearch(labelStore.label_rt, bbox_in_world).forEach(x => {
+    draw_label(d, cameraData, labelStore.labels[x]);
+  });
+}
+
 export class CoastlineLayer implements Layer {
   arcStore: ArcStore;
   labelStore: LabelStore;
@@ -183,6 +328,10 @@ export class CoastlineLayer implements Layer {
     this.arcStore = arcStore;
     this.labelStore = labelStore;
     this.counter = counter;
+  }
+
+  render(rc: RenderCtx) {
+    render(rc, this.arcStore, this.labelStore);
   }
 
   rebuild() {
@@ -246,150 +395,7 @@ export class CoastlineLayer implements Layer {
     return ([] as Target[]).concat(arcts, labts);
   }
 
-  render(rc: RenderCtx) {
-    const { d, cameraData, bbox_in_world, us, mode } = rc;
-    function visible(x: Poly): boolean {
-      if (x.properties.t == "road" && !us.layers.road) return false;
-      if (x.properties.t == "boundary" && !us.layers.boundary) return false;
-      return true;
-    }
 
-    const scale = scale_of_camera(cameraData);
-    const arcs_to_draw_vertices_for: Zpoint[][] = [];
-    const salients: { props: RoadProps, pt: Point }[] = [];
-    const rawFeatures = tsearch(this.arcStore.rt, bbox_in_world).filter(visible);
-    const baseFeatures = sortBy(rawFeatures, x => {
-      let z = 0;
-      const p: PolyProps = x.properties;
-      if (p.t == "natural" && p.natural == "lake") z = 1;
-      if (p.t == "natural" && p.natural == "mountain") z = 1;
-      if (p.t == "city") z = 1;
-      if (p.t == "road" && p.road == "highway") z = 2;
-      if (p.t == "road" && p.road == "street") z = 3;
-      if (p.t == "boundary") z = 4;
-      return z;
-    });
-
-    // Not sure what this is for
-    let extra = baseFeatures.map(x => {
-      if (x.properties.t == "road" && x.properties.road == "street")
-        return { ...x, properties: { ...x.properties, road: "street2" } }
-    }).filter(x => x);
-
-    // Not sure why this cast
-    const features = baseFeatures.concat(extra as any);
-
-    d.save();
-    {
-      canvasIntoWorld(d, cameraData);
-
-      d.strokeStyle = "black";
-      d.lineJoin = "round";
-      features.forEach(ob => {
-        const arc_spec_list = ob.arcs;
-
-        d.lineWidth = 0.9 / scale;
-        d.beginPath();
-
-        const first_point = this.arcStore.getArc(arc_spec_list[0]).points[0].point;
-        d.moveTo(first_point.x, first_point.y);
-
-        let curpoint = first_point;
-        let n = 0;
-
-        arc_spec_list.forEach(spec => {
-          const arc = this.arcStore.getArc(spec);
-          let this_arc = arc.points;
-          let arc_bbox = arc.bbox;
-          if (DEBUG_BBOX) {
-            d.lineWidth = 1.5 / scale;
-            d.strokeStyle = colors.debug;
-            d.strokeRect(arc_bbox.minX, arc_bbox.minY,
-              arc_bbox.maxX - arc_bbox.minX,
-              arc_bbox.maxY - arc_bbox.minY);
-          }
-
-          if (this_arc.length < 2) {
-            throw "arc " + spec + " must have at least two points";
-          }
-
-          const rect_intersect = bbox_in_world[0] < arc_bbox.maxX
-            && bbox_in_world[2] > arc_bbox.minX
-            && bbox_in_world[3] > arc_bbox.minY
-            && bbox_in_world[1] < arc_bbox.maxY;
-
-          if (!rect_intersect) {
-            // The bounding box of this arc is entirely off-screen.
-            // Draw as simplified as possible; just one line segment
-            // from beginning to end.
-            this_arc = [this_arc[0], this_arc[this_arc.length - 1]];
-          }
-
-          if (rect_intersect && zoom_of_camera(cameraData) >= 6) {
-            // draw individual vertices if we're at least partially
-            // on-screen, and also quite zoomed in.
-            arcs_to_draw_vertices_for.push(this_arc);
-          }
-
-          this_arc.forEach(({ point: vert, z }, ix) => {
-            if (ix == 0) return;
-
-            let draw = false;
-
-            // draw somewhat simplified
-            if (zoom_of_camera(cameraData) >= 6 || above_simp_thresh(z, scale))
-              draw = true;
-            if (ix == this_arc.length - 1)
-              draw = true;
-            if (draw) {
-              d.lineTo(vert.x, vert.y);
-              if (ob.properties.t == "road" && ob.properties.road == "highway" && n % 10 == 5) {
-                salients.push({
-                  props: ob.properties,
-                  pt: { x: (vert.x + curpoint.x) / 2, y: (vert.y + curpoint.y) / 2 }
-                });
-              }
-              curpoint = vert;
-              n++;
-            }
-
-          });
-        });
-        realize_path(d, us, ob.properties, cameraData);
-      });
-
-      // draw vertices
-      d.lineWidth = 1.5 / scale;
-      if (mode != "Pan") {
-        d.strokeStyle = "#333";
-        d.fillStyle = "#ffd";
-        let vert_size = 5 / scale;
-        arcs_to_draw_vertices_for.forEach(arc => {
-          arc.forEach(({ point: vert, z }: Zpoint, n: number) => {
-            if (z > 1000000 || zoom_of_camera(cameraData) > 10) {
-              d.fillStyle = z > 1000000 ? "#ffd" : "#f00";
-              d.strokeRect(vert.x - vert_size / 2, vert.y - vert_size / 2, vert_size, vert_size);
-              d.fillRect(vert.x - vert_size / 2, vert.y - vert_size / 2, vert_size, vert_size);
-            }
-          });
-        });
-      }
-    }
-    d.restore();
-
-    // doing this outside the d.save/d.restore because it involves
-    // text, which won't want the negative y-transform
-    salients.forEach(salient => {
-      realize_salient(d, salient.props, cameraData, salient.pt);
-    });
-
-    // render labels
-    if (zoom_of_camera(cameraData) < 1) return;
-    d.lineJoin = "round";
-    tsearch(this.labelStore.label_rt, bbox_in_world).forEach(x => {
-      draw_label(d, cameraData, this.labelStore.labels[x]);
-    });
-  }
 
   // special case first and last of arc??
   replace_vert(targets: Target[], p: Point) {

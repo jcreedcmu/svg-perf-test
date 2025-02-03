d1545341ed26d54e5dff974270c3e595d20da9f8
Jason Reed <jcreed@gmail.com>
2025-02-03 13:50:35 -0500

    Sketch out some nonworking code

--- offline/index.ts
+++ offline/index.ts
@@ -1,11 +1,32 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import { createCanvas } from 'canvas';
 import { render } from '../src/map-canvas-render';
+import { GeoModel, Geometry, Rivers, UiState } from '../src/types';
+import { LabelStore } from '../src/labelstore';
+import { RiverLayer } from '../src/rivers';
+import { SketchLayer } from '../src/sketch';
+import { ArcStore } from '../src/arcstore';
 
 const c = createCanvas(100, 100);
 const d = c.getContext('2d');
 d.fillStyle = 'red';
 d.fillRect(0, 0, 50, 50);
+
+// XXX factor some common code this shares with main.ts
+const geoModel: GeoModel = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/geo.json'), 'utf8'));
+const rivers: Rivers = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/rivers.json'), 'utf8'));
+const geo: Geometry = {
+  arcStore: new ArcStore(geoModel.points, geoModel.arcs, geoModel.polys),
+  images: geoModel.images,
+  labelStore: new LabelStore(geoModel.labels),
+  riverLayer: new RiverLayer(rivers),
+  sketchLayer: new SketchLayer()
+};
+
+const ui: UiState = {} as any; // XXX
+
+render((d as any) as CanvasRenderingContext2D, { geo, ui });
 
 const out = fs.createWriteStream('/tmp/foo.png')
 const stream = c.createPNGStream()
